24d5d675bc78345114a488700c4fe5c310bb21f7
Brandon James Smylie <bsmylie02@qub.ac.uk>
2022-08-20 19:01:09 +0100

    Add files I missed last commit.

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, request, redirect, url_for
+from hangman_game import *
 
 app = Flask(__name__)
-# Load config file.
-app.config.from_pyfile('config.py')
+
+
+def get_db_connection():
+    conn = sqlite3.connect('hangman.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 
 @app.route('/')
-def main():
+def home():
+    conn = get_db_connection()
+    words = conn.execute('SELECT * FROM words').fetchall()
+    conn.close()
     return render_template('index.html')
 
 
