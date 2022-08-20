5c2a0a48118bcedd54838cefc42dc88b996b1521
Brandon James Smylie <bsmylie02@qub.ac.uk>
2022-08-20 15:29:36 +0100

    Added templates folder + html file for quick test.
    Added files to configure environment vairables for MySql server.
    
    Added files to initialise database from existing .sql files.

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+# Load config file.
+app.config.from_pyfile('config.py')
+
+
+@app.route('/')
+def main():
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
-    print("Hello World")
+    app.run(port=40001, debug=True)
---
+++ init_db.py
@@ -0,0 +1,42 @@
+import mysql.connector as mysql
+from mysql.connector import Error
+
+from app import app
+
+app.config.from_pyfile('config.py')
+
+# First, set up the database itself.
+try:
+    db = mysql.connect(
+        host=app.config['DB_HOST'],
+        user=app.config['DB_USER'],
+        passwd=app.config['DB_PASS']
+    )
+
+    with open('db.sql', 'r') as f:
+        with db.cursor() as cursor:
+            cursor.execute(f.read(), multi=True)
+        cursor.close()
+        db.commit()
+# Error handling
+except Error as e:
+    print("Error while connecting to MySql", e)
+
+# Now, set up table inside hangman database.
+try:
+    db = mysql.connect(
+        host=app.config['DB_HOST'],
+        user=app.config['DB_USER'],
+        database="hangman",
+        passwd=app.config['DB_PASS']
+    )
+    print("Connection successful!")
+
+    with open('schema.sql', 'r') as f:
+        with db.cursor() as cursor:
+            cursor.execute(f.read(), multi=True)
+        cursor.close()
+        db.commit()
+# Error handling
+except Error as e:
+    print("Error while connecting to MySql, database created", e)
