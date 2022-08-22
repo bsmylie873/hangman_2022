61ac77446c1858733159c4848aff3147f61abdf9
Brandon James Smylie <bsmylie02@qub.ac.uk>
2022-08-22 23:23:59 +0100

    Day's work has been committed, changes to most import files, basic functionality is now in place.

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import json
 import sqlite3
-import hangman
-from flask import Flask, render_template, request, redirect, url_for, abort, jsonify
+from flask import Flask, render_template, request, redirect, url_for, abort
 from hangman import Hangman
 from random_functions import *
 
 app = Flask(__name__)
-hangman_game = Hangman(game_id=0, word="______", word_length=7, no_of_guesses=0, game_valid=True)
+hangman_game = Hangman(game_id=0, word="______", word_progress="______", word_length=7, no_of_guesses=0,
+                       game_valid=True)
 
 
 @app.route('/')
@@ -24,71 +24,51 @@ def start_game():
         form_data = request.form
         game_id = form_data.get("game_id")
         print("Game ID is " + game_id)
-        cursor.execute("SELECT COUNT(*) FROM Games where Game_ID=?", (game_id,))
+        cursor.execute("SELECT COUNT(*) FROM Games WHERE Game_ID=?", (game_id,))
         result = cursor.fetchone()[0]
 
         if result == 0:
-            cursor.execute("SELECT Word FROM Words where Word_ID=?", (word_id,))
+            cursor.execute("SELECT Word FROM Words WHERE Word_ID=?", (word_id,))
             word_row = list(cursor.fetchone())
             starterStr = "_" * len(word_row[0])
-            sqlite_insert_with_param = """INSERT INTO Games(Word_ID,Status,No_Of_Guesses) VALUES (?,?,?)"""
-            data_tuple = (word_id, starterStr, 0)
+            sqlite_insert_with_param = """INSERT INTO Games(Game_ID,Word_ID,Status,No_Of_Guesses) VALUES (?,?,?,?)"""
+            data_tuple = (game_id, word_id, starterStr, 0)
             cursor.execute(sqlite_insert_with_param, data_tuple)
             conn.commit()
             conn.close()
-
-            global hangman_game
-            no_of_guesses = 0
-            game_valid = True
-            hangman_game = Hangman(game_id=game_id, word=starterStr, word_length=len(word_row[0]),
-                                   no_of_guesses=no_of_guesses, game_valid=game_valid)
-            game_details = json.dumps(hangman_game.__dict__)
-            return redirect("/game/" + hangman_game.id)
-        else:
-            cursor.execute("SELECT * FROM Games where Game_ID=?", (game_id,))
-            result_set = list(cursor.fetchone())
-            game_valid = True
-            hangman_game = Hangman(game_id=result_set[0], word=result_set[1], word_length=len(result_set[1]),
-                                   no_of_guesses=result_set[3], game_valid=game_valid)
-            game_details = json.dumps(hangman_game.__dict__)
-            return redirect("/game/" + hangman_game.id)
     else:
         return abort(400)
-
-
-@app.route("/game/<game_id>", methods=["GET"])
-def get_game_status(game_id):
-    return render_template("game.html")
-
-
-@app.route("/game/<game_id>/<guess>", methods=["POST"])
-def add_char(hangman_game):
-    letter = request.form['letter']
-
-    return hangman_game.process_guess(getattr(hangman_game, 'word'), letter)
-
-
-def update_game_status(data):
     conn = sqlite3.connect('hangman.db')
     cursor = conn.cursor()
-    game_db_data = None
-    if data:
-        game_id = data['game_id']
-        game_status = data['game_status']
-        cursor.execute('UPDATE Games SET Status = ? WHERE Game_ID = ?', (game_status, game_id))
-        conn.commit()
-        cursor.execute('"SELECT Game FROM Games where Game_ID=(%s)"', game_id)
-        game_db_data = list(cursor.fetchone())
-        return game_db_data
+    cursor.execute("SELECT * FROM Games WHERE Game_ID=?", (game_id,))
+    result_set = list(cursor.fetchone())
+    cursor.execute("SELECT * FROM Words WHERE Word_ID=?", (result_set[1],))
+    word_result_set = list(cursor.fetchone())
+    game_valid = True
+    global hangman_game
+    hangman_game = Hangman(game_id=result_set[0], word=word_result_set[1], word_length=word_result_set[2],
+                           word_progress=result_set[2], no_of_guesses=result_set[3], game_valid=game_valid)
+    if hangman_game.check_game_state():
+        return redirect(url_for('game', game_id=game_id))
     else:
-        return game_db_data
+        return render_template("loss.html")
 
 
-# def check_game_status():
-#     if self.check_game_state():
-#         return True
-#     else:
-#         return False
+@app.route("/game/<game_id>/", methods=["GET", "POST"])
+def game(game_id):
+    return render_template("game.html", game_id=game_id, game_details=hangman_game)
+
+
+@app.route("/game/<game_id>/guess", methods=["POST"])
+def add_char(game_id):
+    letter = request.form['letter']
+    hangman_game.process_guess(letter)
+    hangman_game.game_valid = hangman_game.check_game_state()
+
+    if hangman_game.game_valid:
+        return redirect(url_for('game', game_id=game_id, hangman_game=hangman_game))
+    else:
+        return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
--- hangman.py
+++ hangman.py
@@ -1,92 +1,31 @@
-import sqlite3
-
-
 class Hangman:
-    def __init__(self, game_id, word, word_length, no_of_guesses, game_valid):
-        self.id = game_id
+    def __init__(self, game_id, word, word_progress, word_length, no_of_guesses, game_valid):
+        self.game_id = game_id
         self.word = word
+        self.word_progress = word_progress
         self.word_length = word_length
         self.no_of_guesses = no_of_guesses
         self.game_valid = game_valid
 
-    # @property
-    # def id(self):
-    #     return self.id
-    #
-    # @id.setter
-    # def id(self, value):
-    #     self.id = value
-    #
-    # @id.deleter
-    # def id(self):
-    #     del self.id
-    #
-    # @property
-    # def word(self):
-    #     return self.word_id
-    #
-    # @word.setter
-    # def word(self, value):
-    #     self.word_id = value
-    #
-    # @word.deleter
-    # def word(self):
-    #     del self.word
-    #
-    # @property
-    # def word_length(self):
-    #     return self.word_id
-    #
-    # @word_length.setter
-    # def word_length(self, value):
-    #     self.word_length = value
-    #
-    # @word_length.deleter
-    # def word_length(self):
-    #     del self.word_length
-    #
-    # @property
-    # def no_of_guesses(self):
-    #     return self.no_of_guesses
-    #
-    # @no_of_guesses.setter
-    # def no_of_guesses(self, value):
-    #     self.no_of_guesses = value
-    #
-    # @no_of_guesses.deleter
-    # def no_of_guesses(self):
-    #     del self.no_of_guesses
-
     def increment_no_of_guesses(self):
         self.no_of_guesses += 1
 
-    def process_guess(self, guess, to_display):
-        guess = guess.lower()
-        conn = sqlite3.connect('hangman.db')
-        cursor = conn.cursor()
+    def process_guess(self, guess):
+        guess = guess.upper()
+        self.word = self.word.upper()
+        list_of_changes = [None] * self.word_length
+        for index, i in enumerate(self.word):
+            if guess == i:
+                list_of_changes[index] = guess
+            else:
+                list_of_changes[index] = self.word_progress[index]
 
-        cursor.execute("SELECT Word FROM Words where Word_ID=(%s)", self.word_id)
-        result_set = list(cursor.fetchall())
-        result_set[0] = result_set[0].lower()
-        for index, result in enumerate(result_set[0]):
-            if guess == result:
-                to_display[index] = guess
-
-        self.increment_no_of_guesses()
-
-        return to_display
-
-    # @property
-    # def game_valid(self):
-    #     return self.no_of_guesses
-    #
-    # @game_valid.setter
-    # def game_valid(self, value):
-    #     self.game_valid = value
-    #
-    # @game_valid.deleter
-    # def game_valid(self):
-    #     del self.game_valid
+        string = ''.join(str(x) for x in list_of_changes)
+        print(string)
+        if string == self.word_progress:
+            self.increment_no_of_guesses()
+        self.word_progress = string
+        return self.word_progress
 
     def check_game_state(self):
         if self.no_of_guesses >= self.word_length - 1:
